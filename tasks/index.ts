3d7424903c7f51d7db88ef0ffa3653150a662a1c
Abdul Hakim <hakim@undercurrent.tech>
2023-08-13 22:03:42 +0700

    fixed task

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,18 +1,15 @@
 import { task, types } from "hardhat/config";
 import { accounts } from "../scripts/accounts";
 import { deploy, deployAndVerify } from "../scripts/deploy";
-import { Params } from "../scripts/types";
-
-const DefaultParams: Params = {};
 
 task("accounts", "Get list of avalaible accounts").setAction(accounts);
 
 task("deploy", "Deploy contract")
   .addOptionalParam("name", "Contract name", "", types.string)
-  .addOptionalParam("params", "Contract parameters", DefaultParams, types.json)
+  .addOptionalParam("params", "Contract parameters", {}, types.json)
   .setAction(deploy);
 
 task("deploy-and-verify", "Deploy and verify contract")
   .addOptionalParam("name", "Contract name", "", types.string)
-  .addOptionalParam("params", "Contract parameters", DefaultParams, types.json)
+  .addOptionalParam("params", "Contract parameters", {}, types.json)
   .setAction(deployAndVerify);
